d7dd073abe86b6f1791abb689f9322dd2ae13796
Rihan Mohammed <rihanmohammed26@gmail.com>
2022-06-22 16:46:58 +0530

    final commit

--- src/cat/cat.controller.ts
+++ src/cat/cat.controller.ts
@@ -1,4 +1,65 @@
-import { Controller } from '@nestjs/common';
+import {
+    Body,
+    Controller,
+    Delete,
+    Get,
+    HttpCode,
+    HttpStatus,
+    Logger,
+    Param,
+    Post,
+    Put,
+    UseFilters,
+    UsePipes,
+    ValidationPipe,
+} from '@nestjs/common';
+import {
+    ApiBadRequestResponse,
+    ApiBearerAuth,
+    ApiInternalServerErrorResponse,
+    ApiOkResponse,
+    ApiOperation,
+    ApiTags,
+} from '@nestjs/swagger';
+import { CatService } from './cat.service';
+import { CreateCatDto } from './create-cat.dto';
 
-@Controller('cat')
-export class CatController {}
+
+@Controller('cats')
+@UsePipes(
+    new ValidationPipe({
+        whitelist: true,
+        transform: true,
+    }),
+)
+export class CatController {
+    constructor(private readonly catService: CatService) { }
+
+    @ApiTags('cat')
+    @Post('')
+    @ApiOperation({
+        description: 'create new Cat'
+    })
+    @UsePipes(ValidationPipe)
+    @HttpCode(HttpStatus.OK)
+    @ApiOkResponse({ description: 'OK' })
+    @ApiBadRequestResponse({ description: 'Bad Request' })
+    @ApiInternalServerErrorResponse({ description: 'data has been created successfully' })
+    async createCat(@Body() cat: CreateCatDto) {
+        return this.catService.create(cat);
+    }
+
+    @ApiTags('cat')
+    @Get('')
+    @ApiOperation({
+        description: 'Get All Cat'
+    })
+    @UsePipes(ValidationPipe)
+    @HttpCode(HttpStatus.OK)
+    @ApiOkResponse({ description: 'OK' })
+    @ApiBadRequestResponse({ description: 'Bad Request' })
+    @ApiInternalServerErrorResponse({ description: 'data has been fetched successfully' })
+    async listAll() {
+        return this.catService.findAll();
+    }
+}
